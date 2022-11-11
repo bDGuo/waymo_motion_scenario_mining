1437d30b0eb29c617739d027495f6a810ac4486d
BulbasaurG <detian.guo@outlook.com>
2022-11-11 14:53:36 +0100

    refactor:replace os.path with pathlib

--- utils/runner_plot.py
+++ utils/runner_plot.py
@@ -4,25 +4,28 @@ import os
 import time
 import argparse
 from plotting_scenarios import plot_all_scenarios
+from logger.logger import *
+from pathlib import Path
 
-ROOT = os.path.abspath(os.path.dirname(""))
-ROOT = os.path.dirname(ROOT)
-DATADIR = os.path.join(ROOT,"waymo_open_dataset","data","tf_example","training")
-FIGDIR = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(""))),r"figures\scenarios\v2")
+# ROOT = os.path.abspath(os.path.dirname(""))
+# ROOT = os.path.dirname(ROOT)
+# DATADIR = os.path.join(ROOT,"waymo_open_dataset","data","tf_example","training")
+# FIGDIR = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(""))),r"figures\scenarios\v2")
+# RESULTDIR = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(""))),r"results\v4")
 
-
-RESULTDIR = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(""))),r"results\v4")
-RESULT_TIME = f'2022-11-09-09_25'
-
+ROOT = Path(__file__).parent.parent
+DATADIR = ROOT / "waymo_open_dataset/data/tf_example/training"
+FIGDIR = ROOT / "figures/scenarios/v2"
+RESULTDIR = ROOT / "results/v4"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--filenum', type=str, required=True, help='#file to plot.e.g.:00003')
-parser.add_argument('--result_time', type=str, required=True, help='#result time to plot.e.g.:2022-11-09-09_25')
+parser.add_argument('--result_time', type=str, required=True, help='#result time to plot.e.g.:11-09-09_25')
 args = parser.parse_args()
 
 if __name__ == "__main__":
     FILENUM = args.filenum
-    RESULT_TIME = args.result_time
+    RESULT_TIME = f"2022-{args.result_time}"
     FILE = f"training_tfexample.tfrecord-{FILENUM}-of-01000"
     RESULT_FILENAME = f'Waymo_{FILENUM}_{RESULT_TIME}_tag.json'
     RESULT_SOLO = f'Waymo_{FILENUM}_{RESULT_TIME}_solo.json'
@@ -33,4 +36,5 @@ if __name__ == "__main__":
         _=plot_all_scenarios(DATADIR,FILE,FILENUM,RESULTDIR,RESULT_FILENAME,RESULT_SOLO,FIGDIR)
     else:
         print(f"File not found:{FILE}")
+        logger.info(f"File not found:{FILENAME}")
 
