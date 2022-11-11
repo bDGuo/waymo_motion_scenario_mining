57f7a42578a12301a9b83905f6111c0f9ad3e04d
BulbasaurG <detian.guo@outlook.com>
2022-11-11 14:54:47 +0100

    feat: add logging

--- utils/runner.py
+++ utils/runner.py
@@ -11,6 +11,7 @@ import argparse
 from tags_generation import generate_tags
 from mining_scenarios import mine_solo_scenarios
 from rich.progress import track
+from logger.logger import *
 
 ROOT = os.path.abspath(os.path.dirname("__file__"))
 ROOT = os.path.dirname(ROOT)
@@ -26,31 +27,36 @@ FIGUREDIR = os.path.join(ROOT,r"figures\scenarios")
 TTC_1 = 5
 # parameter for estimation of two actors' interaction
 TTC_2 = 9
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--file', type=str, required=True, help='#file to plot.e.g.:00003')
+# args = parser.parse_args()
 
 if __name__ == '__main__':
-    print(ROOT)
     time_start = time.perf_counter()
     for root, dirs, file_list in DATADIR_WALK:
         for FILE in track(file_list,description="Processing files"):
             FILENUM = re.search(r"-(\d{5})-",FILE)
             if FILENUM is not None:
                 FILENUM = FILENUM.group()[1:-1]
-                print("Processing file: {}".format(FILE))
+                print(f"Processing file: {FILE}")
             else:
-                print("File name error: {}".format(FILE))
+                print(f"File name error: {FILE}")
                 continue    
             result_dict = {}
             RESULT_FILENAME = f'Waymo_{FILENUM}_{RESULT_TIME}_tag.json'
-            actors_list,\
-            inter_actor_relation,\
-            actors_activity,\
-            actors_static_element_intersection = generate_tags(DATADIR,FILE)
-            result_dict = {
-            'actors_list':actors_list,
-            'inter_actor_relation':inter_actor_relation,
-            'actors_activity':actors_activity,
-            'actors_static_element_intersection':actors_static_element_intersection
-            }
+            try:
+                actors_list,\
+                inter_actor_relation,\
+                actors_activity,\
+                actors_static_element_intersection = generate_tags(DATADIR,FILE)
+                result_dict = {
+                'actors_list':actors_list,
+                'inter_actor_relation':inter_actor_relation,
+                'actors_activity':actors_activity,
+                'actors_static_element_intersection':actors_static_element_intersection
+                }
+            except Exception as e:
+                logger.error(f"Tag generagtion: {e}")
             with open(os.path.join(RESULTDIR,RESULT_FILENAME),'w') as f:
                 json.dump(result_dict,f)
             # with open(os.path.join(RESULTDIR,RESULT_FILENAME.replace('.json','.pkl')),'wb') as f:
