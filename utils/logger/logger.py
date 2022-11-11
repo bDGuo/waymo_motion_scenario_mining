ea45bd4faf45a178555b75b707bcc59fd5b09a2f
BulbasaurG <detian.guo@outlook.com>
2022-11-11 13:47:39 +0100

    feat:new,file logger

---
+++ utils/logger/logger.py
@@ -0,0 +1,59 @@
+import logging
+import logging.config
+from pathlib import Path
+
+
+log_dir =  Path(__file__).parent / 'log'
+
+log_dir.mkdir(exist_ok=True)
+LOGGING_CONFIG = {
+    "version": 1,
+    "formatters": {
+        "default": {
+            'format':'%(asctime)s %(filename)s %(lineno)s %(levelname)s %(message)s',
+        },
+        "plain": {
+            "format": "%(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "default",
+        },
+        "console_plain": {
+            "class": "logging.StreamHandler",
+            "level":logging.INFO,
+            "formatter": "plain"
+        },
+        "file":{
+            "class": "logging.FileHandler",
+            "level":20,
+            "filename": log_dir/"log.txt",
+            "formatter": "default",
+        }
+    },
+    "loggers": {
+        "console_logger": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "console_plain_logger": {
+            "handlers": ["console_plain"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "file_logger":{
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": False,
+        }
+    },
+    "disable_existing_loggers": True,
+}
+        
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger('file_logger')
