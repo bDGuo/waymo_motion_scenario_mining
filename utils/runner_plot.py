6f1221457501ca5ebb11fd150300051a629a8a64
BulbasaurG <detian.guo@outlook.com>
2022-11-14 10:17:24 +0100

    feat: error and program finishing messager using wechatter

--- utils/runner_plot.py
+++ utils/runner_plot.py
@@ -6,6 +6,7 @@ import argparse
 from plotting_scenarios import plot_all_scenarios
 from logger.logger import *
 from pathlib import Path
+from helpers.wechatter import wechatter
 
 # ROOT = os.path.abspath(os.path.dirname(""))
 # ROOT = os.path.dirname(ROOT)
@@ -16,7 +17,7 @@ from pathlib import Path
 ROOT = Path(__file__).parent.parent
 DATADIR = ROOT / "waymo_open_dataset/data/tf_example/training"
 FIGDIR = ROOT / "figures/scenarios/v2"
-RESULTDIR = ROOT / "results/v4"
+RESULTDIR = ROOT / "results/v6"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--filenum', type=str, required=True, help='#file to plot.e.g.:00003')
@@ -24,6 +25,7 @@ parser.add_argument('--result_time', type=str, required=True, help='#result time
 args = parser.parse_args()
 
 if __name__ == "__main__":
+    start = time.perf_counter()
     FILENUM = args.filenum
     RESULT_TIME = f"2022-{args.result_time}"
     FILE = f"training_tfexample.tfrecord-{FILENUM}-of-01000"
@@ -33,8 +35,17 @@ if __name__ == "__main__":
     FILENAME = os.path.join(DATADIR,FILE)
     if os.path.exists(FILENAME):
         print(f"Plotting:{FILE}")
-        _=plot_all_scenarios(DATADIR,FILE,FILENUM,RESULTDIR,RESULT_FILENAME,RESULT_SOLO,FIGDIR)
+        try:
+            _=plot_all_scenarios(DATADIR,FILE,FILENUM,RESULTDIR,RESULT_FILENAME,RESULT_SOLO,FIGDIR)
+        except Exception as e:
+            wechatter(f"Error in plotting {FILENUM}")
+            logger.error(f"FILE: {FILENUM}.{e}")
     else:
         print(f"File not found:{FILE}")
         logger.info(f"File not found:{FILENAME}")
+    end = time.perf_counter()
+    logger.info(f"DATA:{FILENUM}.JSON:{RESULT_SOLO}.Run time: {end-start}")
+
+    # messager for finishing using wechat. Comment out this if you don't want to use wechat
+    wechatter(f"{FILENUM} Plot finished. Run time: {end-start}")
 
