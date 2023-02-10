e723e432652a7b6c0e1bee64b6606989d0603f6c
BulbasaurG <detian.guo@outlook.com>
2023-02-10 22:04:44 +0100

    initial commit

---
+++ utils/stats/summarize_distribution.py
@@ -0,0 +1,21 @@
+
+from typing import List, Dict, Tuple
+
+def frequency(data:List)->Dict:
+    """
+    Returns a frequency data for the data.
+    """
+    frequency = {}
+    for item in data:
+        frequency[item] = frequency.get(item, 0) + 1
+    return frequency
+
+def pmf(data:List)->Dict:
+    """
+    Returns a probability mass function for the data.
+    """
+    freq = frequency(data)
+    n = len(data)
+    for x in freq:
+        freq[x] /= n
+    return freq
