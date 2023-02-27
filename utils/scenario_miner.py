a0ec12306c9ddfe8d9dc12dded53294893a0a8ff
BulbasaurG <detian.guo@outlook.com>
2023-02-27 11:11:40 +0100

    fix: align with new tag: v_dir

--- utils/scenario_miner.py
+++ utils/scenario_miner.py
@@ -55,9 +55,15 @@ class ScenarioMiner:
                         turning_points = self.__computing_turning_point(value,valid_start,valid_end)
                         if handel == 'position':
                             inter_actor_dict = inter_actor_position_dict
-                        else:
+                        elif handel == 'relation':
                             inter_actor_dict = inter_actor_relation_dict
-                        solo_scenarios[actor_type][agent_key]['inter_actor'][actor_name][handel] = self.__summarizing_events(value,turning_points,valid_start,valid_end,actor_name,inter_actor_dict)
+                        else:
+                            inter_actor_dict = inter_actor_vel_dir_dict
+                        try:
+                            solo_scenarios[actor_type][agent_key]['inter_actor'][actor_name][handel] = self.__summarizing_events(value,turning_points,valid_start,valid_end,actor_name,inter_actor_dict)
+                        except:
+                            print(f"Error: {actor_name} {handel} {value}")
+                            raise
         return solo_scenarios
 
     def __computing_turning_point(self,activity,valid_start:int,valid_end:int)->list:
