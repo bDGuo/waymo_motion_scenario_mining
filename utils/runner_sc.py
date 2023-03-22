3e3413a7393f98eeeaffde1172436882e073adff
BulbasaurG <detian.guo@outlook.com>
2023-03-22 08:55:51 +0100

    fix:eval_mode filename prefix set to SC name.

--- utils/runner_sc.py
+++ utils/runner_sc.py
@@ -10,17 +10,22 @@ from scenario_categorizer import ScenarioCategorizer
 # working directory
 ROOT = Path(__file__).resolve().parent.parent
 parser = argparse.ArgumentParser()
-parser.add_argument('--prefix', help='result file name prefix, e.g. PedestrianCrossing')
-parser.add_argument('--result_time', type=str, required=True, help='result time to be categorized, e.g. 2023-02-28-16_35')
+parser.add_argument('--eval_mode', action="store_true" , help='enable evaluation mode')
+parser.add_argument('--result_time', type=str, required=True, help='result time to be categorized, e.g. 02-28-16_35')
 args = parser.parse_args()
 
 RESULT_TIME = args.result_time
-for i in range(20):
-    FILE = f"0000{i}" if i < 10 else f"000{i}"
-    RESULT_DIR = ROOT / "results/gp1" / RESULT_TIME
-    RESULT_FILE = RESULT_DIR / f"{args.prefix}_{FILE}_{RESULT_TIME}_tag.json"
+RESULT_DIR = ROOT / "results/gp1" / f"2023-{RESULT_TIME}"
+file_prefix = "Waymo" if not args.eval_mode else "Carla"
+for file in RESULT_DIR.iterdir():
+    if not file.name.endswith("tag.json"):
+        continue
+    filenum = file.name.split("_")[1]
+    print(file.name)
+    RESULT_FILE = file
     result_dict = json.load(open(RESULT_FILE, 'r'))
-    scenario_categorizer = ScenarioCategorizer(FILE, result_dict)
+    file_prefix = file.name.split("_")[0] if args.eval_mode else "Waymo"
+    scenario_categorizer = ScenarioCategorizer(filenum, result_dict)
     for SC_ID in ["SC1","SC7","SC13"]:
         try:
             SC_ID_dict = scenario_categorizer.find_SC(SC_ID)
@@ -28,7 +33,7 @@ for i in range(20):
                 continue
             RESULT_SC_DIR = RESULT_DIR / SC_ID
             RESULT_SC_DIR.mkdir(parents=True, exist_ok=True)
-            json.dump(SC_ID_dict, open(RESULT_SC_DIR / f"{args.prefix}_{FILE}_{RESULT_TIME}_{SC_ID}.json", 'w'))
+            json.dump(SC_ID_dict, open(RESULT_SC_DIR / f"{file_prefix}_{filenum}_{RESULT_TIME}_{SC_ID}.json", 'w'))
         except Exception as e:
             trace = traceback.format_exc()
             logger.error(f"SC:{SC_ID}.\nTag generation:{e}")
