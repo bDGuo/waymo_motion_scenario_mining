388ce731dee03a23eae079a953fbb9eac74179a2
BulbasaurG <detian.guo@outlook.com>
2023-05-02 10:19:04 +0200

    feat: new args in argparser for running this script

--- utils/runner_plot.py
+++ utils/runner_plot.py
@@ -11,20 +11,23 @@ from plotting_scenarios import plot_all_scenarios
 # working directory
 ROOT = Path(__file__).parent.parent
 
-# modify the following two lines to your own data,figures, and result directory
-DATADIR = ROOT / "waymo_open_dataset" / "data" / "tf_example" / "training"
-FIGDIR = ROOT / "figures" / "scenarios" / "eval"
 RESULTDIR = ROOT / "results" / "gp1"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--filenum', type=str, required=True, help='#file to plot.e.g.:00003')
 parser.add_argument('--result_time', type=str, required=True, help='#result time to plot.e.g.:11-09-09_25')
 parser.add_argument('--eval_mode', type=bool, required=False, default=False ,help='[bool] True for evaluation mode')
+parser.add_argument('--ext_data',action="store_true" ,help='[bool] True for using external data')
+parser.add_argument('--fig_dir', type=str, required=True, help='directory to save figures, relative to ./figures/scenarios')
 args = parser.parse_args()
 eval_mode = args.eval_mode
+ext_data = args.ext_data
+DATA_DIR = ROOT / "waymo_open_dataset" / "data" / "tf_example" / "training"
+if ext_data:
+    DATA_DIR = Path("E:/VRU_prediction_dataset/waymo")
 if eval_mode:
-    DATADIR = ROOT / "waymo_open_dataset" / "data" / "eval_data" / "carla_data"
-
+    DATA_DIR = ROOT / "waymo_open_dataset" / "data" / "eval_data" / "carla_data"
+FIGDIR = ROOT / "figures" / "scenarios" / args.fig_dir
 
 if __name__ == "__main__":
     start = time.perf_counter()
@@ -38,11 +41,11 @@ if __name__ == "__main__":
         RESULT_FILENAME = f'Carla_{FILENUM}_{RESULT_TIME}_tag.json'
         RESULT_SOLO = f'Carla_{FILENUM}_{RESULT_TIME}_solo.json'
     # sanity check
-    FILE_PATH = DATADIR / FILE
+    FILE_PATH = DATA_DIR / FILE
     if FILE_PATH.exists():
         print(f"Plotting:{FILE}")
         try:
-            _=plot_all_scenarios(DATADIR,FILE,FILENUM,RESULTDIR / RESULT_TIME,RESULT_FILENAME,RESULT_SOLO,FIGDIR,eval_mode=eval_mode)
+            _=plot_all_scenarios(DATA_DIR,FILE,FILENUM,RESULTDIR / RESULT_TIME,RESULT_FILENAME,RESULT_SOLO,FIGDIR,eval_mode=eval_mode)
         except Exception as e:
             ##########################################
             # messager for finishing one data record. Comment out this if you don't use wechat
