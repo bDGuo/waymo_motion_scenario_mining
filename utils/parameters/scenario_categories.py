d228e4bce727de07cf20a282d713376ccfba7918
BulbasaurG <detian.guo@outlook.com>
2023-05-22 10:42:30 +0200

    chore: delete SCs for development.

--- utils/parameters/scenario_categories.py
+++ utils/parameters/scenario_categories.py
@@ -78,76 +78,10 @@ class Car2CarFrontTurn(SCBasis):
 
 
 @dataclass
-class Car2BycFrontTurn(Car2CarFrontTurn):
-    """
-    Inherit from Car2CarFrontTurn
-    """
-    #####   general info    #####
-    SC_ID = "SC5"
-    #####   guest actor  #####
-    guest_actor_type = ["cyclist"]
-
-
-@dataclass
-class Car2PedFrontTurn(Car2CarFrontTurn):
-    """
-    Inherit from Car2CarFrontTurn
-    """
-    #####   general info    #####
-    SC_ID = "SC8"
-    #####   guest actor  #####
+class Car2PedCrossStraight(SCBasis):
+    SC_ID = "SC_7"
+    description = "Car-to-pedestrian_crossing_straight_crossing_path"
     guest_actor_type = ["pedestrian"]
-
-
-@dataclass
-class Car2CarFrontHeadon(SCBasis):
-    #####   general info    #####
-    SC_ID = "SC3"
-    description = "Car-to-car_front_headon"
-    source = "EURO_NCAP_2023"
-    source_file = "euro-ncap-aeb-lss-vru-test-protocol-v43.pdf"
-    #####   host actor  #####
-    host_actor_type = ["vehicle"]
-    host_actor_tag = {
-        "lo_act": ['accelerating', 'cruising', 'decelerating'],  # forward
-        'la_act': ['swerving left', 'swerving right', 'going straight'],
-        "road_relation": [],
-        "road_type": [],
-        "inter_actor_relation": ['estimated collision', 'close proximity', "estimated collision+close proximity"],
-        "inter_actor_position": ['front'],
-        'inter_actor_heading': ['opposite']
-    }
-    #####   guest actor  #####
-    guest_actor_type = ["vehicle"]
-    guest_actor_tag = {
-        "lo_act": ['accelerating', 'cruising', 'decelerating'],
-        "la_act": ['swerving left', 'swerving right', 'going straight'],
-        "road_relation": [],
-        "road_type": [],
-        "inter_actor_relation": [],
-        "inter_actor_position": []
-    }
-
-
-@dataclass
-class Car2PedFrontHeadon(Car2CarFrontHeadon):
-    SC_ID = "SC_10"
-    description = "Car-to-pedestrian_front_headon"
-    guest_actor_type = ["pedestrian"]
-
-
-@dataclass
-class Car2BycFrontHeadon(Car2CarFrontHeadon):
-    SC_ID = "SC_4"
-    description = "Car-to-cyclist_front_headon"
-    guest_actor_type = ["cyclist"]
-
-
-@dataclass
-class Car2CarCrossStraight(SCBasis):
-    #####   general info    #####
-    SC_ID = "SC2"
-    description = "Car-to-car_crossing_straight_crossing_path"
     source = "EURO_NCAP_2023"
     source_file = "euro-ncap-aeb-lss-vru-test-protocol-v43.pdf"
     #####   host actor  #####
@@ -170,45 +104,6 @@ class Car2CarCrossStraight(SCBasis):
         "road_type": [],
         "inter_actor_relation": [],
         "inter_actor_position": []
-    }
-
-
-@dataclass
-class Car2PedCrossStraight(Car2CarCrossStraight):
-    SC_ID = "SC_7"
-    description = "Car-to-pedestrian_crossing_straight_crossing_path"
-    guest_actor_type = ["pedestrian"]
-
-
-@dataclass
-class Car2BycCrossStraight(Car2CarCrossStraight):
-    SC_ID = "SC_12"
-    description = "Car-to-cyclist_crossing_straight_crossing_path"
-    guest_actor_type = ["cyclist"]
-
-
-@dataclass
-class CarViolateTrafficLight(SCBasis):
-    #####   general info    #####
-    SC_ID = "SC11"
-    description = "Car violates traffic light"
-    source = ""
-    source_file = ""
-    #####   host actor  #####
-    host_actor_type = ["vehicle"]
-    host_actor_tag = {
-        "lo_act": [],
-        'la_act': [],
-        "road_relation": ['staying', 'entering', 'leaving'],
-        "road_type": ['controlled_lane'],
-        "inter_actor_relation": [],
-        "inter_actor_position": []
-    }
-    #####   guest actor  #####
-    # skip
-    #####    environment  #####
-    envr_tag = {
-        'light_state': ['Arrow stop', 'Stop', 'Flashing stop']
     }
 
 @dataclass
@@ -244,17 +139,6 @@ class Car2BycPassingby(SCBasis):
 
 scenario_catalog = {
     "SC1": Car2CarFrontTurn,
-    "SC5": Car2BycFrontTurn,
-    "SC8": Car2PedFrontTurn,
-
-    "SC3": Car2CarFrontHeadon,
-    "SC10": Car2PedFrontHeadon,
-    "SC4": Car2BycFrontHeadon,
-
-    "SC11": CarViolateTrafficLight,
-    
-    "SC2": Car2CarCrossStraight,
     "SC7": Car2PedCrossStraight,
-
     "SC13": Car2BycPassingby,
 }
