fceb99e15940eed4bdb31f694e6daae88263eeaf
BulbasaurG <detian.guo@outlook.com>
2023-05-22 10:42:52 +0200

    chore: move the sleeper function here.

--- utils/helpers/helper_func.py
+++ utils/helpers/helper_func.py
@@ -1,5 +1,11 @@
 from typing import Dict
+import time
 
 
 def exchange_key_value(old_dict: Dict) -> Dict:
     return dict([val, key] for key, val in old_dict.items())
+
+def sleeper(sleep_time:int):
+    for i in range(sleep_time):
+        time.sleep(1)
+        print(f"Sleeping {i+1}/{sleep_time} seconds...")
